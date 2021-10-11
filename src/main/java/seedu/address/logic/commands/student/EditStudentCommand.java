3389a24ca93cb962f352fb83aa75407686af3343
nniiggeell <e0559733@u.nus.edu>
2021-10-11 17:33:39 +0800

    Refactor EditStudentCommand.java to a student package

--- src/main/java/seedu/address/logic/commands/student/EditStudentCommand.java
+++ src/main/java/seedu/address/logic/commands/student/EditStudentCommand.java
@@ -1,4 +1,4 @@
-package seedu.address.logic.commands;
+package seedu.address.logic.commands.student;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
@@ -16,6 +16,8 @@ import java.util.List;
 
 import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.CommandResult;
+import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.descriptors.EditStudentDescriptor;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
