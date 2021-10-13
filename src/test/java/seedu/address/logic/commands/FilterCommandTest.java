ecf4bfd0c9228a48614079d50a2b34a5f07054aa
Clive <e0531337@u.nus.edu>
2021-10-14 01:50:05 +0800

    Add filter by tag and involvement
    New format is
    ```
    filter [INVOLVEMENT_FILTER_CATERGORY]... [/t] [TAG_CATEGORY]...
    ```

--- src/test/java/seedu/address/logic/commands/FilterCommandTest.java
+++ src/test/java/seedu/address/logic/commands/FilterCommandTest.java
@@ -5,13 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.commons.core.Messages.MESSAGE_PERSONS_LISTED_OVERVIEW;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.testutil.TypicalPersons.ALICE;
 import static seedu.address.testutil.TypicalPersons.BENSON;
-import static seedu.address.testutil.TypicalPersons.CARL;
-import static seedu.address.testutil.TypicalPersons.DANIEL;
-import static seedu.address.testutil.TypicalPersons.ELLE;
-import static seedu.address.testutil.TypicalPersons.FIONA;
-import static seedu.address.testutil.TypicalPersons.GEORGE;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import java.util.Arrays;
@@ -69,13 +63,13 @@ public class FilterCommandTest {
     }
 
     @Test
-    public void execute_multipleKeywords_multiplePersonsFound() {
-        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 7);
-        InvolvementContainsKeywordsPredicate predicate = preparePredicate("Form class");
+    public void execute_multipleKeywords_multiplePersonsFound2() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 1);
+        InvolvementContainsKeywordsPredicate predicate = preparePredicate("class t/owesmoney");
         FilterCommand command = new FilterCommand(predicate);
         expectedModel.updateFilteredPersonList(predicate);
         assertCommandSuccess(command, model, expectedMessage, expectedModel);
-        assertEquals(Arrays.asList(ALICE, BENSON, CARL, DANIEL, ELLE, FIONA, GEORGE), model.getFilteredPersonList());
+        assertEquals(Arrays.asList(BENSON), model.getFilteredPersonList());
     }
 
     /**
