df1632205393df871ef0133bb1f95f2c29b35f68
random689 <freezerfiregithub@gmail.com>
2021-10-18 23:15:34 +0800

    Add new exception for Teacher and Student Add TeacherNotFoundException.java Add DuplicateTeacherException.java Add StudentNotFoundException.java Add DuplicateStudentException.java

---
+++ src/main/java/seedu/address/model/person/teacher/exceptions/TeacherNotFoundException.java
@@ -0,0 +1,8 @@
+package seedu.address.model.person.teacher.exceptions;
+
+import seedu.address.model.person.exceptions.PersonNotFoundException;
+
+/**
+ * Signals that the operation is unable to find the specified Teacher.
+ */
+public class TeacherNotFoundException extends PersonNotFoundException {}
