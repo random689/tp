572b63559303e8dca604508988e64e1ac516dd78
nniiggeell <e0559733@u.nus.edu>
2021-10-20 22:42:43 +0800

    Add test class for ShowMedicalWindowCommandParser

---
+++ src/test/java/seedu/address/logic/parser/student/ShowMedicalHistoryCommandParserTest.java
@@ -0,0 +1,38 @@
+package seedu.address.logic.parser.student;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_STUDENT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_STUDENT;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.student.FindStudentCommand;
+import seedu.address.logic.commands.student.ShowMedicalHistoryCommand;
+import seedu.address.model.person.student.StudentNameContainsKeywordsPredicate;
+
+class ShowMedicalHistoryCommandParserTest {
+    private final ShowMedicalHistoryCommandParser parser = new ShowMedicalHistoryCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsShowMedicalHistoryCommand() {
+        assertParseSuccess(parser, "1", new ShowMedicalHistoryCommand(INDEX_FIRST_STUDENT));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                ShowMedicalHistoryCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_emptyArg_throwsParseException() {
+        assertParseFailure(parser, "     ", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                ShowMedicalHistoryCommand.MESSAGE_USAGE));
+    }
+}
