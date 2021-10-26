71583cb2a58d41c51f693bffb64112301af75373
nniiggeell <e0559733@u.nus.edu>
2021-10-26 15:45:04 +0800

    Add QuitMeetingCommand.java
    To quit the meeting window.

---
+++ src/main/java/seedu/address/logic/commands/QuitMeetingCommand.java
@@ -0,0 +1,15 @@
+package seedu.address.logic.commands;
+
+import seedu.address.model.Model;
+
+public class QuitMeetingCommand extends Command {
+    public static final String COMMAND_WORD = "quitMeeting";
+
+    public static final String MESSAGE_QUIT_ACKNOWLEDGEMENT = "Exiting meeting window as requested, and opening main "
+            + "window";
+
+    @Override
+    public CommandResult execute(Model model) {
+        return new CommandResult(MESSAGE_QUIT_ACKNOWLEDGEMENT, false, false, false, false, true);
+    }
+}
