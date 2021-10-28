de608358023d830e11d0bbe1e034b5011b11dcb2
Clive <e0531337@u.nus.edu>
2021-10-28 21:53:03 +0800

    Change format of filter

--- src/main/java/seedu/address/logic/parser/teacher/FilterTeacherCommandParser.java
+++ src/main/java/seedu/address/logic/parser/teacher/FilterTeacherCommandParser.java
@@ -2,7 +2,8 @@ package seedu.address.logic.parser.teacher;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 import seedu.address.logic.commands.teacher.FilterTeacherCommand;
 import seedu.address.logic.parser.Parser;
@@ -15,6 +16,7 @@ import seedu.address.model.person.teacher.TeacherInvolvementContainsKeywordsPred
 
 public class FilterTeacherCommandParser implements Parser<FilterTeacherCommand> {
 
+    private static final String TAG_REGEX = "^[a-zA-Z0-9]*$";
     /**
      * Parses the given {@code String} of arguments in the context of the FilterTeacherCommand
      * and returns a FilterTeacherCommand object for execution.
@@ -29,7 +31,61 @@ public class FilterTeacherCommandParser implements Parser<FilterTeacherCommand> 
 
         String[] nameKeywords = trimmedArgs.split("\\s+");
 
-        return new FilterTeacherCommand(new TeacherInvolvementContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        List<String> processedNameKeywords = processInput(nameKeywords);
+
+        return new FilterTeacherCommand(new TeacherInvolvementContainsKeywordsPredicate(processedNameKeywords));
+    }
+
+
+    private List<String> processInput(String[] keywords) throws ParseException {
+        boolean isTagsOnwards = false;
+        List<String> listToReturn = new ArrayList<>();
+        for (int i = 0; i < keywords.length; i++) {
+            if (!isTagsOnwards) {
+                if (keywords[i].startsWith("t/") && validFirstTag(keywords[i])) {
+                    isTagsOnwards = true;
+                    listToReturn.add(keywords[i]);
+                } else if (keywords[i].startsWith("t/") && !validFirstTag(keywords[i])) {
+                    isTagsOnwards = true;
+                    throw new ParseException(
+                            String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterTeacherCommand.MESSAGE_USAGE));
+                } else {
+                    listToReturn.add(keywords[i]);
+                }
+            } else {
+                if (validOtherTag(keywords[i])) {
+                    listToReturn.add(keywords[i].substring(2));
+                } else {
+                    throw new ParseException(
+                            String.format(MESSAGE_INVALID_COMMAND_FORMAT, FilterTeacherCommand.MESSAGE_USAGE));
+                }
+            }
+        }
+
+        return listToReturn;
+    }
+
+    private boolean validFirstTag(String keyword) {
+        if (keyword.length() <= 2) {
+            return false;
+        }
+        if (keyword.substring(2).matches(TAG_REGEX)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean validOtherTag(String keyword) {
+        if (!keyword.startsWith("t/")) {
+            return false;
+        } else if (keyword.length() <= 2) {
+            return false;
+        } else if (keyword.substring(2).matches(TAG_REGEX)) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
